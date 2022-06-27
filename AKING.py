c732a46dd43a7b0c4c82bc5b83dac27b689aa0b8
SecDet <96675285+C4LLM3D3V1L@users.noreply.github.com>
2022-06-27 18:51:56 +0600

    Create AKING.py

---
+++ AKING.py
@@ -0,0 +1,30 @@
+import os, platform
+def psb(z):
+
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.09)
+
+
+try:
+   import requests
+except:
+   os.system('pip2 install requests')
+
+import requests,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    psb('\n\n\n\n\n\033[92;1m  AKING BYPASS IN PROGRESS...!')
+    time.sleep(2)
+    os.system('clear')
+    psb('\n\n\n\n\n\033[91;1m BYPASS BY SecDet...!')
+    time.sleep(6)
+    from adnan import readline___Public_Xml
+    readline___Public_Xml()
+elif bit == '32bit':
+    os.system('clear')
+    print ('YOUR DEVICE IS 32 BIT')
+    print ('')
+    print ('UPGRADE YOUR DEVICE AND TRY AGAIN')
